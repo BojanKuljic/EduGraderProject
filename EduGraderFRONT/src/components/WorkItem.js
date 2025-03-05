f034e6d36d7552fd8726637a4d5e826e4bbaade3
BojanKuljic <bojankuljic16@gmail.com>
2025-03-05 11:45:32 +0100

    Initial commit for EduGraderProject

---
+++ EduGraderFRONT/src/components/WorkItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function WorkItem({ title, description }) {
+  return (
+    <div className="work-item">
+      <h3>title</h3>
+      <p>{dsescription}</p>
+    </div>
+  );
+}
+
+export default WorkItem;
