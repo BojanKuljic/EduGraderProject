c6f1aa1541ed4f5730e1a8ce50c396f14e9460e5
KuljicBojan <bojan.kuljic@mozzartbet.com>
2025-08-02 02:05:04 +0200

    Add on server side in Common models for SystemSettings and Interface updates

---
+++ EduGraderBACK/Common/Models/SystemSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class SystemSettings
+    {
+        public string AnalysisMethod { get; set; }
+        public string EvaluationStyle { get; set; }
+        public string Language { get; set; }
+        public int MaxUploads { get; set; }
+        public string Period { get; set; }
+    }
+}
